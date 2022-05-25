e06ebf9c99af26c2549478993468a05a2b579348
黄才进 <huangcaijin@ycyykj.com>
2022-05-25 14:43:04 +0800

    导出

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 let StrPlay = require("./StrPlay")
-
+// console.log(DatePlay)
 // let a = new StrPlay('<h1>this</h1>  <span>is</span> <br>  <i>test</i> string 你好世界!')
 // console.log(a.space())
 // console.log(a.space().tags())
 // console.log(a.space().tags().cwords())
 // console.log(a.space().tags().cwords().cat("one"))
 // console.log(a.space().tags().cwords().cat("one").cut())
+if(window){
+    window.StrPlay = StrPlay
+}
+module.exports = StrPlay
